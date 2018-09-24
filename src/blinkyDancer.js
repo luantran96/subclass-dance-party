1376f74d50d092f2cd1de9ed8464fa7225df7b64
luantran96 <luantran96@yahoo.com>
2018-09-24 15:20:33 -0700

    Actually done refactoring

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -11,12 +11,12 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
   BlinkyDancer.prototype = Object.create(Dancer.prototype);
   BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  //BlinkyDancer.prototype.oldStep = BlinkyDancer.prototype.step;
+  BlinkyDancer.prototype.oldStep = BlinkyDancer.prototype.step;
 
 
   BlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    Dancer.prototype.step.call(this);
+    this.oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
